10ec18dd55461c74652a70fee0cded18ae095338
Luca Iovio <luca.iovio@gmail.com>
2020-11-19 11:55:45 +0100

    added tests

--- src/test.py
+++ src/test.py
@@ -1,4 +1,15 @@
 from client import get_data
 
+def check_data():
+    res = get_data()
+
+    for item in res:
+        if type(item['first_name']) != str:
+            return False
+        if type(item['last_name']) != str:
+            return False
+
+    return True
+
 def test_get_data():
-    assert get_data()
+    assert check_data()
